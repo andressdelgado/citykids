636becbccc4c33bed72464bcbdcfedd75b2b3e08
andressdelgado <andresdelgadoherruzo@gmail.com>
2023-11-15 12:48:04 +0100

    mvc funcionando

--- src/js/vistas/vista3.js
+++ src/js/vistas/vista3.js
@@ -8,11 +8,22 @@ export class Vista3 extends Vista{
         //Coger referecnias del interfaz
         this.enlace1 = this.base.querySelectorAll('button')[0]
         this.enlace2 = this.base.querySelectorAll('button')[1]
-        this.enlace3 = this.base.querySelectorAll('button')[2]
+
         //Asociar eventos
         this.enlace1.onclick = this.pulsarEnlace1.bind(this)
         this.enlace2.onclick = this.pulsarEnlace2.bind(this)
-        this.enlace3.onclick = this.pulsarEnlace3.bind(this)
+        //CREA EL INTERFAZ
+        this.crearInterfaz()
+    }
+    crearInterfaz(){
+        //SE CREA EL BOTON DINAMICAMENTE QUE LLEVA A LA VISTA1
+        this.btnVerVista1 = document.createElement('button')
+        this.base.appendChild(this.btnVerVista1)
+        this.btnVerVista1.textContent = 'VOLVER AL MENÚ'
+        this.btnVerVista1.className = 'volverAlJuego'
+        this.btnVerVista1.onclick = () => {
+            this.controlador.verVista(Vista.VISTA1)
+        }
     }
     //Funciones para ver la vista que corresponde segun el boton accionado
     pulsarEnlace1(){
