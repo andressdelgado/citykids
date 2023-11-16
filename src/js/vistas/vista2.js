88a833e66a9cde080dbd382df9a38b12a38baf00
andressdelgado <andresdelgadoherruzo@gmail.com>
2023-11-16 10:32:02 +0100

    Solucion Pulsa Enter Vista2

--- src/js/vistas/vista2.js
+++ src/js/vistas/vista2.js
@@ -14,45 +14,45 @@ export class Vista2 extends Vista {
 
         // Inicializa un generador de números
         this.generadorNumeros = new GeneradorNumeros();
-
+        document.addEventListener('keydown', this.cambiarVista.bind(this))
         // Agrega el event listener para el evento 'keydown' al divVista2
-        this.divVista2.addEventListener('keydown', (event) => {
-            if (event.key === 'Enter') {
-                // Reemplaza el alert con el código para obtener números aleatorios
-                const numero = this.generadorNumeros.obtenerNumeroAleatorio();
-                if (numero === 0) {
-                    console.log("Ya se proporcionaron todos los números.");
-                } else {
-                    console.log("Número obtenido:", numero);
-                    switch (numero) {
-                        case 1:
-                            console.log("Participacion democractica")
-                            this.controlador.verVista(Vista.VISTA9);
-                            break;
-                        case 2:
-                            console.log("Justicia Social")
-                            this.controlador.verVista(Vista.VISTA10);
-                            break;
-                        case 3:
-                            console.log("Desarrollo Humano y Sostenible")
-                            this.controlador.verVista(Vista.VISTA11);
-                            break;
-                        case 4:
-                            console.log("Interculturalidad e inclusión");
-                            this.controlador.verVista(Vista.VISTA12);
-                            break;
-                        case 5:
-                            console.log("Equidad de género y coeducación");
-                            this.controlador.verVista(Vista.VISTA13);
-                            break;
-                        default:
-                            console.log("Número no válido");
-                    }
-                    
-
+    }
+    cambiarVista(event){
+        if (event.key === 'Enter') {
+            // Reemplaza el alert con el código para obtener números aleatorios
+            const numero = this.generadorNumeros.obtenerNumeroAleatorio();
+            if (numero === 0) {
+                console.log("Ya se proporcionaron todos los números.");
+            } else {
+                console.log("Número obtenido:", numero);
+                switch (numero) {
+                    case 1:
+                        console.log("Participacion democractica")
+                        this.controlador.verVista(Vista.VISTA9);
+                        break;
+                    case 2:
+                        console.log("Justicia Social")
+                        this.controlador.verVista(Vista.VISTA10);
+                        break;
+                    case 3:
+                        console.log("Desarrollo Humano y Sostenible")
+                        this.controlador.verVista(Vista.VISTA11);
+                        break;
+                    case 4:
+                        console.log("Interculturalidad e inclusión");
+                        this.controlador.verVista(Vista.VISTA12);
+                        break;
+                    case 5:
+                        console.log("Equidad de género y coeducación");
+                        this.controlador.verVista(Vista.VISTA13);
+                        break;
+                    default:
+                        console.log("Número no válido");
                 }
+                
+
             }
-        });
+        }
     }
 }
 
