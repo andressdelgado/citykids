12141ddc34b325b9643fa5a4e28dc3683362f3ac
andressdelgado <andresdelgadoherruzo@gmail.com>
2023-11-17 12:06:54 +0100

    vista comenntada para eslint

--- src/js/vistas/vista.js
+++ src/js/vistas/vista.js
@@ -1,31 +1,57 @@
-//AQUI SE CREAN LAS VISTAS COMO SIMBOLOS, Y SE LES DA UN NOMBRE
-export class Vista{
-    static{
-        Vista.VISTA1 = Symbol('Inicio')
-        Vista.VISTA2 = Symbol('Juego')
-        Vista.VISTA3 = Symbol('Menu Competicion')
-        Vista.VISTA4 = Symbol('Crear Competicion')
-        Vista.VISTA5 = Symbol('Unirse Competicion')
-        Vista.VISTA6 = Symbol('Menu Ranking')
-        Vista.VISTA7 = Symbol('Ranking Global')
-        Vista.VISTA8 = Symbol('Ranking Competiciones')
-        Vista.VISTA9 = Symbol('Participación Democrática')
-        Vista.VISTA10 = Symbol('Justicia Social')
-        Vista.VISTA11 = Symbol('Desarrollo Humano y Sostenible')
-        Vista.VISTA12 = Symbol('Interculturalidad e Inclusión')
-        Vista.VISTA13 = Symbol('Equidad de género y Coeducación')
+/**
+ * Clase base para la creación de vistas como símbolos con nombres asociados.
+ * @class
+ */
+export class Vista {
+    /**
+     * Enumeración de símbolos asociados a nombres de vistas.
+     * @enum {Symbol}
+     */
+    static {
+        Vista.VISTA1 = Symbol('Inicio');
+        Vista.VISTA2 = Symbol('Juego');
+        Vista.VISTA3 = Symbol('Menu Competicion');
+        Vista.VISTA4 = Symbol('Crear Competicion');
+        Vista.VISTA5 = Symbol('Unirse Competicion');
+        Vista.VISTA6 = Symbol('Menu Ranking');
+        Vista.VISTA7 = Symbol('Ranking Global');
+        Vista.VISTA8 = Symbol('Ranking Competiciones');
+        Vista.VISTA9 = Symbol('Participación Democrática');
+        Vista.VISTA10 = Symbol('Justicia Social');
+        Vista.VISTA11 = Symbol('Desarrollo Humano y Sostenible');
+        Vista.VISTA12 = Symbol('Interculturalidad e Inclusión');
+        Vista.VISTA13 = Symbol('Equidad de género y Coeducación');
     }
-    constructor(controlador, base){
-        this.controlador = controlador
-        this.base = base
+
+    /**
+     * Constructor de la clase Vista.
+     * @constructor
+     * @param {Controlador} controlador - Referencia al controlador de la aplicación.
+     * @param {HTMLElement} base - Elemento HTML que representa la base de la vista.
+     */
+    constructor(controlador, base) {
+        /**
+         * Referencia al controlador de la aplicación.
+         * @type {Controlador}
+         */
+        this.controlador = controlador;
+
+        /**
+         * Elemento HTML que representa la base de la vista.
+         * @type {HTMLElement}
+         */
+        this.base = base;
     }
-    /*Muestra u oculta la vista.
-    * @param ver {Boolean} Indica si la vista debe mostrarse (true) u ocultar (false)
-    */
-    mostrar(ver){
-        if(ver)
-            this.base.style.display = 'block'
-        else
-            this.base.style.display = 'none'
+
+    /**
+     * Muestra u oculta la vista.
+     * @param {Boolean} ver - Indica si la vista debe mostrarse (true) u ocultarse (false).
+     */
+    mostrar(ver) {
+        if (ver) {
+            this.base.style.display = 'block';
+        } else {
+            this.base.style.display = 'none';
+        }
     }
 }
