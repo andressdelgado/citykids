c1df82cd8e69eae2c31cf124feffd5c9726757d3
andressdelgado <andresdelgadoherruzo@gmail.com>
2023-11-17 11:50:44 +0100

    controlador comentado para eslint

--- src/js/scriptmvc.js
+++ src/js/scriptmvc.js
@@ -1,28 +1,32 @@
-import {Modelo} from './modelos/modelo.js'
-import {Vista} from './vistas/vista.js'
-import {Vista1} from './vistas/vista1.js'
-import {Vista2} from './vistas/vista2.js'
-import {Vista3} from './vistas/vista3.js'
-import {Vista4} from './vistas/vista4.js'
-import {Vista5} from './vistas/vista5.js'
-import {Vista6} from './vistas/vista6.js'
-import {Vista7} from './vistas/vista7.js'
-import {Vista8} from './vistas/vista8.js'
-import {Vista9} from './vistas/vista9.js'
-import {Vista10} from './vistas/vista10.js'
-import {Vista11} from './vistas/vista11.js'
-import {Vista12} from './vistas/vista12.js'
-import {Vista13} from './vistas/vista13.js'
-class Controlador{
-    /*
-    * Incializa los atributos.
-    * Coge las preferencias del interfaz.
-    */
-    vistas = new Map()
-    constructor(){
-        this.modelo = new Modelo()
+import { Modelo } from './modelos/modelo.js';
+import { Vista } from './vistas/vista.js';
+import { Vista1 } from './vistas/vista1.js';
+import { Vista2 } from './vistas/vista2.js';
+import { Vista3 } from './vistas/vista3.js';
+import { Vista4 } from './vistas/vista4.js';
+import { Vista5 } from './vistas/vista5.js';
+import { Vista6 } from './vistas/vista6.js';
+import { Vista7 } from './vistas/vista7.js';
+import { Vista8 } from './vistas/vista8.js';
+import { Vista9 } from './vistas/vista9.js';
+import { Vista10 } from './vistas/vista10.js';
+import { Vista11 } from './vistas/vista11.js';
+import { Vista12 } from './vistas/vista12.js';
+import { Vista13 } from './vistas/vista13.js';
 
-        //Consigo las referencias del interfaz
+/**
+ * Clase principal que controla la lógica de la aplicación.
+ */
+class Controlador {
+    /**
+     * Inicializa los atributos y crea instancias de modelos y vistas.
+     */
+    vistas = new Map();
+    constructor() {
+        /** @type {Modelo} - La instancia del modelo de la aplicación. */
+        this.modelo = new Modelo();
+
+        // Consigue las referencias del interfaz
         const divVista1 = document.getElementById('divVista1')
         const divVista2 = document.getElementById('divVista2')
         const divVista3 = document.getElementById('divVista3')
@@ -37,7 +41,7 @@ class Controlador{
         const divVista12 = document.getElementById('divVista12')
         const divVista13 = document.getElementById('divVista13')
 
-        //Creo las vistas
+        // Crea instancias de vistas y las agrega al mapa de vistas
         this.vistas.set(Vista.VISTA1, new Vista1(this,divVista1))
         this.vistas.set(Vista.VISTA2, new Vista2(this,divVista2))
         this.vistas.set(Vista.VISTA3, new Vista3(this,divVista3))
@@ -52,17 +56,22 @@ class Controlador{
         this.vistas.set(Vista.VISTA12, new Vista12(this,divVista12))
         this.vistas.set(Vista.VISTA13, new Vista13(this,divVista13))
 
-
-        this.verVista(Vista.VISTA1)
+        // Muestra la primera vista al cargar la página
+        this.verVista(Vista.VISTA1);
     }
+
+    /**
+     * Validar los campos del formulario.
+     * @returns {boolean} - Retorna true si el formulario es válido, de lo contrario, retorna false.
+     */
     validarFormulario() {
-        //Obtener referencias a los campos del formulario
+        // Obtener referencias a los campos del formulario
         const clave = document.getElementById('crearclave').value;
         const titulo = document.getElementById('creartitulo').value;
         const descripcion = document.getElementById('creardescripcion').value;
         const fechaFin = document.getElementById('crearfechaFin').value;
 
-        //Expresiones regulares para validación
+        // Expresiones regulares para validación
         const claveRegex = /^[A-Za-z]{3}[A-Za-z0-9]{2,9}$/;
         const tituloRegex = /^[A-Za-z ]{5,100}$/;
         const descripcionRegex = /^[A-Za-z0-9 ]{0,255}$/; // puede ser nulo
@@ -70,13 +79,13 @@ class Controlador{
 
         // Realizar validaciones
         if (!claveRegex.test(clave)) {
-            alert('Clave no válida. Debe empezar al menos por 3 caracteres de letras y como mínimo 5 caracteres generales. Además, como máximo tendrá 10. No se permiten espacios.');
+            alert('Clave no válida. Debe empezar al menos por 3 caracteres de letras y como mínimo 5 caracteres generales. No se permiten espacios.');
             return false;
         }
 
         if (!tituloRegex.test(titulo)) {
-             alert('Título no válido. Debe contener al menos 5 caracteres de letras y como máximo 100. No se permiten otros caractéres');
-             return false;
+            alert('Título no válido. Debe contener al menos 5 caracteres de letras y como máximo 100. No se permiten otros caracteres.');
+            return false;
         }
 
         if (!descripcionRegex.test(descripcion)) {
@@ -85,21 +94,34 @@ class Controlador{
         }
 
         if (!fechaRegex.test(fechaFin)) {
-            alert('Fecha no utilizada. Utiliza una fecha.');
+            alert('Fecha no válida. Utiliza una fecha.');
             return false;
         }
+
         //SI TODAS LAS COMPROBACIONES ESTAN CORRECTAS
         alert('Formulario válido. Puedes enviarlo o realizar otras acciones según tu lógica.');
     }
-    verVista(vista){
-        this.ocultarVistas()
-        this.vistas.get(vista).mostrar(true)
-    }
-    ocultarVistas(){
-        for(let vista of this.vistas.values())
-            vista.mostrar(false)
+
+    /**
+     * Muestra la vista deseada.
+     * @param {string} vista - El nombre de la vista a mostrar.
+     */
+    verVista(vista) {
+        this.ocultarVistas();
+        this.vistas.get(vista).mostrar(true);
     }
 
+    /**
+     * Oculta todas las vistas.
+     */
+    ocultarVistas() {
+        for (let vista of this.vistas.values()) {
+            vista.mostrar(false);
+        }
+    }
 }
 
-window.onload = () => {new Controlador()}
+// Inicializa el controlador cuando la página se carga completamente
+window.onload = () => {
+    new Controlador();
+};
--- src/js/vistas/vista2.js
+++ src/js/vistas/vista2.js
@@ -27,11 +27,19 @@ export class Vista2 extends Vista {
         this.pEnter.textContent = 'CLICA EN LA PANTALLA Y LUEGO PULSA ENTER PARA GIRAR LA RULETA'
         this.pEnter.className = 'volverAlJuego'
     }
+    crearInterfaz2(){
+        //SE CREA EL BOTON DINAMICAMENTE QUE LLEVA A LA VISTA1
+        this.pEnter = document.createElement('p')
+        this.base.appendChild(this.pEnter)
+        this.pEnter.textContent = 'Ya se han proporcionado todos los ambitos, pulsa f5 y mira los otros apartados. (Esta parte está en desarrollo ya que aqui irán las preguntas)'
+        this.pEnter.className = 'volverAlJuego'
+    }
     cambiarVista(event){
         if (event.key === 'Enter') {
             const numero = this.generadorNumeros.obtenerNumeroAleatorio();
             if (numero === 0) {
                 console.log("Ya se proporcionaron todos los números.");
+                this.crearInterfaz2()
             } else {
                 console.log("Número obtenido:", numero);
                 //this.controlador.verVista(Vista.VISTA+numero);
