2a00e5a8916deb05cad5ec2ad9db61169a7e8f31
andressdelgado <andresdelgadoherruzo@gmail.com>
2023-11-17 12:17:51 +0100

    Vista 13 comentada para eslint

--- src/js/vistas/vista13.js
+++ src/js/vistas/vista13.js
@@ -1,17 +1,30 @@
-import {Vista} from './vista.js'
+import { Vista } from './vista.js';
 
-//Esta es la vista de equidad de género y coeducación
+/**
+ * Clase que representa la decimotercera vista de la aplicación.
+ * En esta vista, se aborda el tema de la equidad de género y coeducación.
+ * @extends Vista
+ */
+export class Vista13 extends Vista {
+    /**
+     * Constructor de la clase Vista13.
+     * @param {Controlador} controlador - Referencia al controlador de la aplicación.
+     * @param {HTMLElement} base - Elemento HTML que representa la base de la vista.
+     */
+    constructor(controlador, base) {
+        super(controlador, base);
 
-export class Vista13 extends Vista{
-    constructor(controlador, base){
-        super(controlador, base)
-        //Coger referecnias del interfaz
-        this.enlace1 = this.base.querySelectorAll('button')[0]
-        //Asociar eventos
-        this.enlace1.onclick = this.pulsarEnlace1.bind(this)
+        // Coger referencias del interfaz
+        this.enlace1 = this.base.querySelectorAll('button')[0];
+
+        // Asociar eventos
+        this.enlace1.onclick = this.pulsarEnlace1.bind(this);
     }
-    //Funciones para ver la vista que corresponde segun el boton accionado
-    pulsarEnlace1(){
-        this.controlador.verVista(Vista.VISTA2)
-    } 
+
+    /**
+     * Muestra la Vista2 (juego) al hacer clic en el enlace.
+     */
+    pulsarEnlace1() {
+        this.controlador.verVista(Vista.VISTA2);
+    }
 }
