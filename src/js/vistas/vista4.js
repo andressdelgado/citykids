3cdeec8285de8b409c5ae483f4b4ec6568ac5e45
andressdelgado <andresdelgadoherruzo@gmail.com>
2023-11-17 12:11:36 +0100

    Viestas 3 y 4 comentdas para eslint

--- src/js/vistas/vista4.js
+++ src/js/vistas/vista4.js
@@ -1,8 +1,16 @@
 import { Vista } from './vista.js';
 
-// Esta es la vista en la que se crea una competición
-
+/**
+ * Clase que representa la cuarta vista de la aplicación.
+ * En esta vista se crea una competición.
+ * @extends Vista
+ */
 export class Vista4 extends Vista {
+    /**
+     * Constructor de la clase Vista4.
+     * @param {Controlador} controlador - Referencia al controlador de la aplicación.
+     * @param {HTMLElement} base - Elemento HTML que representa la base de la vista.
+     */
     constructor(controlador, base) {
         super(controlador, base);
 
@@ -12,18 +20,25 @@ export class Vista4 extends Vista {
 
         // Asociar eventos
         this.enlace2.onclick = this.pulsarEnlace2.bind(this);
-        //AÑADE UN EVENTO CUANDO SE CLICA EN EL SUBMIT DEL FORMULARIO QUE LLEVA A LA FUNCION DE VALIDAR FORMULARIO
-        this.formulario.onclick = this.formulario.addEventListener('submit', (event) => this.validarFormulario(event));
+        this.formulario.addEventListener('submit', (event) => this.validarFormulario(event));
     }
 
+    /**
+     * Muestra la Vista1 (menú principal) al hacer clic en el segundo enlace.
+     */
     pulsarEnlace2() {
         this.controlador.verVista(Vista.VISTA1);
     }
 
+    /**
+     * Valida el formulario al hacer clic en el botón de envío.
+     * @param {Event} event - Objeto de evento asociado al clic del botón de envío del formulario.
+     */
     validarFormulario(event) {
-        //QUITA EL EVENTO POR DEFECTO
+        // Prevenir el envío automático del formulario
         event.preventDefault();
-        //LLAMA A LA FUNCION VALIDAR FORMULARIO DEL CONTROLADOR
+
+        // Llamar a la función validarFormulario del controlador
         this.controlador.validarFormulario();
     }
 }
