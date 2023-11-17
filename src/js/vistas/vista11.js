c19433c558ab2edcab8795b9b75ae84de436c108
andressdelgado <andresdelgadoherruzo@gmail.com>
2023-11-17 12:16:07 +0100

    Vistas 11 y 12 comentadas para eslint

--- src/js/vistas/vista11.js
+++ src/js/vistas/vista11.js
@@ -1,17 +1,30 @@
-import {Vista} from './vista.js'
+import { Vista } from './vista.js';
 
-//Esta es la vista de desarrollo humano y sostenible
+/**
+ * Clase que representa la undécima vista de la aplicación.
+ * En esta vista, se aborda el tema del desarrollo humano y sostenible.
+ * @extends Vista
+ */
+export class Vista11 extends Vista {
+    /**
+     * Constructor de la clase Vista11.
+     * @param {Controlador} controlador - Referencia al controlador de la aplicación.
+     * @param {HTMLElement} base - Elemento HTML que representa la base de la vista.
+     */
+    constructor(controlador, base) {
+        super(controlador, base);
 
-export class Vista11 extends Vista{
-    constructor(controlador, base){
-        super(controlador, base)
-        //Coger referecnias del interfaz
-        this.enlace1 = this.base.querySelectorAll('button')[0]
-        //Asociar eventos
-        this.enlace1.onclick = this.pulsarEnlace1.bind(this)
+        // Coger referencias del interfaz
+        this.enlace1 = this.base.querySelectorAll('button')[0];
+
+        // Asociar eventos
+        this.enlace1.onclick = this.pulsarEnlace1.bind(this);
     }
-    //Funciones para ver la vista que corresponde segun el boton accionado
-    pulsarEnlace1(){
-        this.controlador.verVista(Vista.VISTA2)
-    } 
+
+    /**
+     * Muestra la Vista2 (juego) al hacer clic en el enlace.
+     */
+    pulsarEnlace1() {
+        this.controlador.verVista(Vista.VISTA2);
+    }
 }
--- src/js/vistas/vista12.js
+++ src/js/vistas/vista12.js
@@ -1,17 +1,30 @@
-import {Vista} from './vista.js'
+import { Vista } from './vista.js';
 
-//Esta es la vista de interculturalidad e inclusión
+/**
+ * Clase que representa la duodécima vista de la aplicación.
+ * En esta vista, se aborda el tema de la interculturalidad e inclusión.
+ * @extends Vista
+ */
+export class Vista12 extends Vista {
+    /**
+     * Constructor de la clase Vista12.
+     * @param {Controlador} controlador - Referencia al controlador de la aplicación.
+     * @param {HTMLElement} base - Elemento HTML que representa la base de la vista.
+     */
+    constructor(controlador, base) {
+        super(controlador, base);
 
-export class Vista12 extends Vista{
-    constructor(controlador, base){
-        super(controlador, base)
-        //Coger referecnias del interfaz
-        this.enlace1 = this.base.querySelectorAll('button')[0]
-        //Asociar eventos
-        this.enlace1.onclick = this.pulsarEnlace1.bind(this)
+        // Coger referencias del interfaz
+        this.enlace1 = this.base.querySelectorAll('button')[0];
+
+        // Asociar eventos
+        this.enlace1.onclick = this.pulsarEnlace1.bind(this);
     }
-    //Funciones para ver la vista que corresponde segun el boton accionado
-    pulsarEnlace1(){
-        this.controlador.verVista(Vista.VISTA2)
-    } 
+
+    /**
+     * Muestra la Vista2 (juego) al hacer clic en el enlace.
+     */
+    pulsarEnlace1() {
+        this.controlador.verVista(Vista.VISTA2);
+    }
 }
