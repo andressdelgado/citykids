1ff6b9bfa4df45e31500070621c51efac0bdfb32
andressdelgado <andresdelgadoherruzo@gmail.com>
2023-12-14 13:44:48 +0100

    css arreglado, boton añadido

--- src/js/vistas/vpersonajes.js
+++ src/js/vistas/vpersonajes.js
@@ -4,11 +4,18 @@ export class Personajes extends Vista {
 
   constructor (controlador, base) {
     super(controlador, base)
-    this.volverAlJuego = this.base.querySelectorAll('button')[0]
-    this.volverAlJuego.onclick = this.pulsarbtnVolver.bind(this)
-}
+    this.crearInterfaz()
+  }
 
-    pulsarbtnVolver () {
-    this.controlador.verVista(Vista.vmenuinicial)
+  crearInterfaz () {
+    this.btnVolverJuego = document.createElement('button')
+    this.base.appendChild(this.btnVolverJuego)
+    this.btnVolverJuego.textContent = 'VOLVER AL MENÚ'
+    this.btnVolverJuego.className = 'volverAlJuego'
+
+    // Asociar evento al botón para volver al menú principal (vMenuInicial)
+    this.btnVolverJuego.onclick = () => {
+      this.controlador.verVista(Vista.vmenuinicial)
     }
+  }
 }
